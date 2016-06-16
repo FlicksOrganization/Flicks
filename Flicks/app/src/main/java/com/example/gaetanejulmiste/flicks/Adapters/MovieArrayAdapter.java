d3c2e8c4dfd6d7ea792964514f5eef247391a17c
Gaetane Julmiste <gaetanejulmiste@Gaetanes-MacBook-Pro.local>
2016-06-16 10:09:38 -0500

    Optimized Layout with ViewHolder

--- Flicks/app/src/main/java/com/example/gaetanejulmiste/flicks/Adapters/MovieArrayAdapter.java
+++ Flicks/app/src/main/java/com/example/gaetanejulmiste/flicks/Adapters/MovieArrayAdapter.java
@@ -19,6 +19,16 @@ import java.util.List;
  */
 public class MovieArrayAdapter extends ArrayAdapter<Movie>{
 
+
+    private static class ViewHolder {
+        ImageView ivImage ;
+        TextView tvTitle ;
+        TextView tvOverview ;
+
+
+    }
+
+
     public MovieArrayAdapter(Context context, List<Movie> movieList) {
         super(context, android.R.layout.simple_list_item_1,movieList);
     }
@@ -26,20 +36,31 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie>{
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Movie movie = getItem(position);
+        ViewHolder viewHolder;
         if (convertView == null){
+            viewHolder = new ViewHolder();
             LayoutInflater inflater =LayoutInflater.from(getContext());
             convertView= inflater.inflate(R.layout.item_movie,parent,false);
-
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvDescription);
+            viewHolder.tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+           convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+        viewHolder.tvTitle.setText(movie.getPosterTitle());
+        viewHolder.tvOverview.setText(movie.getOverView());
+
+      /*
+      ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
         ivImage.setImageResource(0);
         TextView tvTitle = (TextView) convertView.findViewById(R.id.tvDescription);
         TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
         tvTitle.setText(movie.getPosterTitle());
         tvOverview.setText(movie.getOverView());
-
-        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+        */
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImage);
 
 
         return convertView;
