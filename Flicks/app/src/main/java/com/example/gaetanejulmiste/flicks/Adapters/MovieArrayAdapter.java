febd9439d24c7c11232f187bdd3e1ca2060054ee
Gaetane Julmiste <gaetou1089@yahoo.fr>
2016-06-27 14:30:53 -0500

    Wireframe details abouts the movies

--- Flicks/app/src/main/java/com/example/gaetanejulmiste/flicks/Adapters/MovieArrayAdapter.java
+++ Flicks/app/src/main/java/com/example/gaetanejulmiste/flicks/Adapters/MovieArrayAdapter.java
@@ -2,6 +2,7 @@ package com.example.gaetanejulmiste.flicks.Adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
